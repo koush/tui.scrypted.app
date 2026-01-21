a9d07c7e29c9dce67a2c330f767ef497091e20e4
Koushik Dutta <koushd@gmail.com>
2026-01-21 15:28:25 -0800

    theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -15,13 +15,15 @@ import '../fonts/inter-4/inter.css';
 import '../styles/quicksand.css';
 import '../styles/vp-doc.css';
 import { createVuetify } from 'vuetify';
+import { themes } from './vuetify-themes';
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     defaultTheme: 'system',
+    themes,
   },
-    icons: {
+  icons: {
     defaultSet: 'fa',
     aliases,
     sets: {
