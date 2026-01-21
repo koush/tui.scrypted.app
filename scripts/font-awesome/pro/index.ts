376b33aa1e128fa32d9477682cf2222032c15c5f
Koushik Dutta <koushd@gmail.com>
2026-01-21 14:55:25 -0800

    missing files

---
+++ scripts/font-awesome/pro/index.ts
@@ -0,0 +1,8 @@
+import { setFaPrefix } from '../../../src/common/fa-prefix'
+import '@fortawesome/fontawesome-pro/css/fontawesome.min.css'
+import './light.css'
+import './solid.css'
+import './regular.css';
+import './brands.css'
+
+setFaPrefix('fa-light');
